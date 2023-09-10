80e631dc8b81d647bc0df703eb557acfb54d653f
Sunday Gbenga <sunflizasher@gmail.com>
2023-09-10 21:11:50 +0100

    Adding slack

--- endpoint.js
+++ endpoint.js
@@ -4,19 +4,20 @@ const router = express.Router();
 const moment = require('moment');
 
 const app = express();
-app.use(express.json());
-app.use(express.cors());
+// app.use(express.json());
+// app.use(express.cors());
 
 app.get('/endpoint', (req, res) => {
 	// res.send('This is working');
 	// const slack_name = req.query.slack_name;
 	// const track_name = req.query.track;
+	const slack_name = 'Sunfliz';
 	const track_name = 'Backend';
 	const data = {
-		slackName: track_name,
+		slackName: slack_name,
 		currentDay: moment().format('dddd'),
 		utcTIme: moment().format('YYYY-MM-DDTHH:mm:ssZ'),
-		track: `${track_name}`,
+		track: track_name,
 		githubFileUrl:
 			'https://github.com/sudaygbenga/hng-backend-internship-task/endpoint.js',
 		githubRepoUrl: 'https://github.com/sudaygbenga/hng-backend-internship-task',
