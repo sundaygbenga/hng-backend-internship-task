41ec88d804ffb456e72c288e648e58c04b50052a
Sunday Gbenga <sunflizasher@gmail.com>
2023-09-10 21:46:20 +0100

    Request Update

--- endpoint.js
+++ endpoint.js
@@ -4,20 +4,17 @@ const router = express.Router();
 const moment = require('moment');
 
 const app = express();
-// app.use(express.json());
-// app.use(express.cors());
 
-app.get('/endpoint', (req, res) => {
-	// res.send('This is working');
-	// const slack_name = req.query.slack_name;
-	// const track_name = req.query.track;
-	const slack_name = 'Sunfliz';
-	const track_name = 'Backend';
+app.get('/api', (req, res) => {
+	const slack_name = req.query.slack_name;
+	const track = req.query.track;
+	// const slack_name = 'Sunfliz';
+	// const track_name = 'Backend';
 	const data = {
 		slackName: slack_name,
 		currentDay: moment().format('dddd'),
 		utcTIme: moment().format('YYYY-MM-DDTHH:mm:ssZ'),
-		track: track_name,
+		track: track,
 		githubFileUrl:
 			'https://github.com/sudaygbenga/hng-backend-internship-task/endpoint.js',
 		githubRepoUrl: 'https://github.com/sudaygbenga/hng-backend-internship-task',
