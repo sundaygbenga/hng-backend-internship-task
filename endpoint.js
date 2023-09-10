14c18f7ba6bde5a05c5b822896d96b014d3ef603
Sunday Gbenga <sunflizasher@gmail.com>
2023-09-10 21:07:23 +0100

    Project Commit

--- endpoint.js
+++ endpoint.js
@@ -1,13 +1,22 @@
+const fs = require('fs/promises');
 const express = require('express');
 const router = express.Router();
 const moment = require('moment');
 
-router.get('/', (req, res) => {
+const app = express();
+app.use(express.json());
+app.use(express.cors());
+
+app.get('/endpoint', (req, res) => {
+	// res.send('This is working');
+	// const slack_name = req.query.slack_name;
+	// const track_name = req.query.track;
+	const track_name = 'Backend';
 	const data = {
-		slackName: 'Sunfliz',
+		slackName: track_name,
 		currentDay: moment().format('dddd'),
 		utcTIme: moment().format('YYYY-MM-DDTHH:mm:ssZ'),
-		track: 'Backend Track',
+		track: `${track_name}`,
 		githubFileUrl:
 			'https://github.com/sudaygbenga/hng-backend-internship-task/endpoint.js',
 		githubRepoUrl: 'https://github.com/sudaygbenga/hng-backend-internship-task',
@@ -16,4 +25,7 @@ router.get('/', (req, res) => {
 
 	res.json(data);
 });
-module.exports = router;
+
+app.listen(3500, () => console.log('API Server is running...'));
+
+module.exports = app;
